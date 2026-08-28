9d556a36cb3f7c5fbef48dd11620bbffdc7d2df5
Saumya Kesur <saumyakesur@gmail.com>
2026-08-28 22:11:04 +0530

    Enable DEBUG mode temporarily on Render for error tracking

--- resume_comparator/settings.py
+++ resume_comparator/settings.py
@@ -131,7 +131,7 @@ import os
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
-    DEBUG = False
+    DEBUG = True
     
     # Secure the key via env
     SECRET_KEY = os.environ.get('SECRET_KEY', SECRET_KEY)
